2214c8fc0a83ec0ec1f83dd502a7503a40e7e5ba
Daniar <dakimaliev@gmail.com>
2024-11-22 18:48:54 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers= Arrays.asList(4,7,1,6,8,9,3,12,11);
+        System.out.println("Четные числа: ");
+        OptionalDouble average=numbers.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).average();
+        System.out.println(average.getAsDouble());
+
+    }
+}
