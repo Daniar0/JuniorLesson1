efb40f1ea455523f69d660ca5914fab6d9f089c4
Daniar <dakimaliev@gmail.com>
2024-11-22 18:53:10 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> numbers= Arrays.asList(4,7,1,6,8,9,3,12,11);
-        System.out.println("Четные числа: ");
+        System.out.println("Среднее значение всех четных чисел: ");
         OptionalDouble average=numbers.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).average();
         System.out.println(average.getAsDouble());
 
